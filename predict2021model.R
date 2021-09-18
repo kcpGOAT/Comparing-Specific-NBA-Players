7e077393d1e068cfa691dce6f1009c35d00b5f1a
kcpGOAT <90441353+kcpGOAT@users.noreply.github.com>
2021-09-17 22:01:31 -0700

    Update predict2021model.R

--- predict2021model.R
+++ predict2021model.R
@@ -1,18 +1,15 @@
-# This program attempts to predict the TS% of a player for the 2020-2021 NBA season based on a linear model. 
+## This program attempts to predict the TS% of a player for the 2020-2021 NBA season based on a linear model. 
 library(tidyverse)
 names(nba_data_historical)[15] <- "TS"
 
 model2021_TS <- function(player) {
-  # This creates a dataset that only includes regular season TS along with name and years
   player_data <- nba_data_historical %>%
     filter(type == "RS", name_common == player) %>%
     select(name_common, year_id, TS) %>%
     arrange(year_id)
-  # This creates a linear model between year and TS%
   player_regression <- lm(TS ~ year_id, data = player_data)
-  # This adds the TS% from 2020 to the slope from the regression, and then combines that result to a percentage sign. 
-  paste(round(player_data$TS[player_data$year_id == 2020] 
-        + unname(coef(player_regression)[2]), digits = 1), "%", sep = "")
+  noquote(paste(round(player_data$TS[player_data$year_id == 2020] 
+                      + unname(coef(player_regression)[2]), digits = 1), "%", sep = ""))
 }
 
 model2021_TS("Trae Young")
@@ -20,8 +17,8 @@ model2021_TS("Luka Doncic")
 model2021_TS("Giannis Antetokounmpo")
 
 # > model2021_TS("Trae Young")
-# [1] "65.1%"
+# [1] 65.1%
 # > model2021_TS("Luka Doncic")
-# [1] "62.5%"
+# [1] 62.5%
 # > model2021_TS("Giannis Antetokounmpo")
-# [1] "63.1%"
+# [1] 63.1%
